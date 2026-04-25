9800f2590739f1c3952852af809b62a447fa63a2
jaykel1jaykel <jaykelmirandaa@gmail.com>
2026-04-25 00:28:11 -0600

    se creo  el archivo funciones.py
    se creo el archivo funciones.py donde van a  estar las funciones principales de la tarea programada

---
+++ Funciones.py
@@ -0,0 +1,9 @@
+# Elaborado por:
+# Fecha de creacion:
+# Ultima modificacion:
+# version de python:
+
+# Funciones principales para la tarea programada de traducir codigo
+def cargarTokens():
+    archivo = input("ingrese el nombre del archivo")
+    
